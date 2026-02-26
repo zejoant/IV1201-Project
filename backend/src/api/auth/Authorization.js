bc2e9c9da6aa1c9e5aec3479ffbf00689e3a08d9
Isak Hanserkers <isahan@kth.se>
2026-02-26 12:26:01 +0100

    Attempt at Authorization logging

--- backend/src/api/auth/Authorization.js
+++ backend/src/api/auth/Authorization.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const jwt = require("jsonwebtoken");
+const logger = require("../../utils/logger");
+
 
 class Authorization{
 
@@ -8,6 +10,7 @@ class Authorization{
         const token = jwt.sign({id: person.person_id, username: person.username}, process.env.JWT_SECRET, {expiresIn: "1h"})
         const options = {httpOnly: true, maxAge: 60 * 60 * 1000};
         res.cookie("auth", token, options)
+        logger.info(`A cookie was sent for username=${person.username}, id=${person.person_id}`);
     }
 
     static async checkLogin(req, res) {
@@ -15,6 +18,7 @@ class Authorization{
 
         if (!authCookie) {
             res.status(401).json({ message: "cookie not found" });
+            logger.info(`authCookie not found for request=${req}`);
             return false;
         }
         try {
@@ -22,10 +26,12 @@ class Authorization{
 
             // decoded = { id, username, iat, exp }
             req.user = decoded;
+            logger.info(`Token successfully verified for request=${req}`);
 
             return true;
         } catch (err) {
             res.status(401).json({ message: "Invalid token" });
+            logger.error(`An error occured when checking login cookie: error=${err}`);
             return false;
         }
     }
