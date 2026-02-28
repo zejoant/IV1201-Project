c28241170cf41017366ce190bf1140cad6197f70
ludwigfl <williamflodin.4@gmail.com>
2026-02-28 12:55:08 +0100

    ci-cd hotfix
    Co-Authored-By: Anton Hammar <77977238+zejoant@users.noreply.github.com>
    Co-Authored-By: Isak-Hanserkers <65658783+Isak-Hanserkers@users.noreply.github.com>

--- frontend/vitest.config.mjs
+++ frontend/vitest.config.mjs
@@ -1,3 +1,4 @@
+// vitest.config.mjs
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
@@ -8,5 +9,8 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: fileURLToPath(new URL('./tests/setupTest.js', import.meta.url)),
+    deps: {
+      inline: ['@exodus/bytes'], // force ESM dependency to be bundled by Vite
+    },
   },
 });
