119435899a7a19dfeb92010856c02e4fd26ab973
ludwigfl <williamflodin.4@gmail.com>
2026-02-28 12:51:19 +0100

    more ci-cd fixes
    Co-Authored-By: Anton Hammar <77977238+zejoant@users.noreply.github.com>
    Co-Authored-By: Isak-Hanserkers <65658783+Isak-Hanserkers@users.noreply.github.com>

--- frontend/vitest.config.mjs
+++ frontend/vitest.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './tests/setupTest.js',
+    setupFiles: fileURLToPath(new URL('./tests/setupTest.js', import.meta.url)),
   },
 });
